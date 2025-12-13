3c405e275e2e4a1b02de23b66a9b406ad6ab7046
mariiagrm <maria.garciam10@um.es>
2025-12-13 15:20:26 +0100

    mailserver sin autenticacion TLS

--- S1_DMZ/Correo/certificados/crearCertificados.sh
+++ S1_DMZ/Correo/certificados/crearCertificados.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")" || exit 1
-
-DOMINIO="mail.dorayaki.org"
+DOMINIO="server.dorayaki.org"
+DOMINIOCORREO="mail.dorayaki.org"
 DIAS=365
 KEYBITS=4096
 
@@ -10,4 +10,15 @@ openssl req -x509 -newkey rsa:${KEYBITS} -nodes \
     -keyout ./privkey.pem \
     -out ./fullchain.pem \
     -sha256 -days "${DIAS}" \
-    -subj "/C=ES/ST=Murcia/L=Murcia/O=DorayakiOrg/OU=IT/CN=${DOMINIO}"
+    -subj "/C=ES/ST=Murcia/L=Murcia/O=DorayakiOrg/OU=IT/CN=${DOMINIOCORREO}" \
+    -addext "subjectAltName = DNS:${DOMINIO}"
+
+#Para certificados autofirmados 
+#si no existe certbot, instalar con: sudo apt install certbot
+if ! command -v certbot &> /dev/null
+then
+    echo "certbot could not be found, installing..."
+    sudo apt install certbot -y
+fi
+
+sudo certbot certonly --standalone -d ${DOMINIO} -d ${DOMINIOCORREO}
